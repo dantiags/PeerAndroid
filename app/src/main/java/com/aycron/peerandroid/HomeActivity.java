54c0e35a472b31bc4aaaa8e056a24e38437a764a
carlos.dantiags <dantiags@gmail.com>
2016-06-30 15:14:10 -0300

    More Activities commit....

--- app/src/main/java/com/aycron/peerandroid/HomeActivity.java
+++ app/src/main/java/com/aycron/peerandroid/HomeActivity.java
@@ -2,12 +2,42 @@ package com.aycron.peerandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity  implements OnClickListener{
+
+    ImageButton resultsButton;
+    ImageButton mapsButton;
+    ImageButton messagesButton;
+    ImageButton menuButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        resultsButton = (ImageButton) findViewById(R.id.imgMenuResults);
+        mapsButton = (ImageButton) findViewById(R.id.imgMenuMap);
+        messagesButton = (ImageButton) findViewById(R.id.imgMenuMessages);
+        menuButton = (ImageButton) findViewById(R.id.imgMenuMenu);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgMenuResults:
+                break;
+            case R.id.imgMenuMap:
+                break;
+            case R.id.imgMenuMessages:
+                break;
+            case R.id.imgMenuMenu:
+                break;
+            
+        }
     }
 }
